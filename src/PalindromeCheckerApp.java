78bbac19f349eafcb92665165f209db07ee19be1
idkduckme <vg1132@srmist.edu.in>
2026-02-18 16:26:19 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,30 +4,21 @@ import java.util.Deque;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        // Define the input string
         String input = "refer";
 
-        // Create a Deque to store characters
         Deque<Character> deque = new ArrayDeque<>();
-
-        // Add each character to the deque
         for (char c : input.toCharArray()) {
             deque.addLast(c);
         }
 
-        // Flag to track palindrome result
         boolean isPalindrome = true;
 
-        // Continue comparison while more than one element exists
         while (deque.size() > 1) {
-            // Remove from both ends and compare
             if (!deque.removeFirst().equals(deque.removeLast())) {
                 isPalindrome = false;
                 break;
             }
         }
-
-        // Display results
         System.out.println("Input String: " + input);
         if (isPalindrome) {
             System.out.println("Result: The input string is a confirmed palindrome.");
